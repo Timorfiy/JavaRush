8a025ad318fd9050d99c6f0c51d111f605b78c77
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-18 19:42:16 +0500

    Add files via upload

---
+++ lesson-11.5.1/src/Solution.java
@@ -0,0 +1,17 @@
+/*
+Сравнение строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String s1 = new String("JavaRush");
+        s1 = "JavaRush";
+        String s2 = "JavaRush";
+//        s2 = new String("JavaRush");
+        String s3 = "JavaRush";
+//        s3 = new String("JavaRush");
+        System.out.println(s1 == s3);
+        System.out.println(s2.equals(s3));
+    }
+}
