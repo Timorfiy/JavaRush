a164b6679d4d7f476500cf41d4453656edaa4686
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-18 16:57:19 +0500

    Add files via upload

---
+++ lesson-11.4.1/src/Bugatti.java
@@ -0,0 +1,23 @@
+/*
+С крышей или без? Вот в чем вопрос
+*/
+
+public class Bugatti {
+    private String color = "BLACK";
+    private int year = 2020;
+    private String body = "Coupe";
+
+    //напишите тут ваш код
+
+    public Bugatti() {
+
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
