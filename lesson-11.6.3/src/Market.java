29afb390f3ec5df456300f9afc605dfa7dcd21e6
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-20 22:17:32 +0500

    Add files via upload

---
+++ lesson-11.6.3/src/Market.java
@@ -0,0 +1,20 @@
+/*
+Купи 10 батонов
+*/
+
+public class Market {
+    private static boolean hasEggs = false;
+
+    public static void main(String[] args) {
+        boolean hasEggs = false;
+        makePurchases(hasEggs);
+    }
+
+    public static void makePurchases(boolean hasEggs) {
+        if (hasEggs) {
+            System.out.println("Купил 10 батонов");
+        } else {
+            System.out.println("Купил 1 батон");
+        }
+    }
+}
