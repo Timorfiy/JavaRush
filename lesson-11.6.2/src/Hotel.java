4f8915a4490b65f9c6e305847b7e17e1b22a8bf0
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-20 22:07:37 +0500

    Add files via upload

---
+++ lesson-11.6.2/src/Hotel.java
@@ -0,0 +1,10 @@
+/*
+Расставь правильно этажи
+*/
+
+public class Hotel {
+    public int thirdFloor = 3;
+    public int fourthFloor = thirdFloor + 1;
+    public int fifthFloor = fourthFloor + 1;
+    public int sixthFloor = fifthFloor + 1;
+}
