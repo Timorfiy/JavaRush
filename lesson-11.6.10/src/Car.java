4a481cd300cd0f6d3f28198580b8e15eab2e82d8
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-21 23:56:57 +0500

    Add files via upload

---
+++ lesson-11.6.10/src/Car.java
@@ -0,0 +1,16 @@
+/*
+Автопроизводитель
+*/
+
+public class Car {
+    private String model;
+    private int year;
+    private String color;
+
+    //напишите тут ваш код
+    public void initialize(String model, int year, String color) {
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+}
