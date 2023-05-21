8b2474b57e4dadd18c15e958e0ad452f8a42a88e
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-21 23:48:38 +0500

    Add files via upload

---
+++ lesson-11.6.9/src/Computer.java
@@ -0,0 +1,6 @@
+public class Computer {
+    Keyboard keyboard = new Keyboard();
+    Monitor monitor = new Monitor();
+    Mouse mouse = new Mouse();
+    SystemUnit systemUnit = new SystemUnit();
+}
---
+++ lesson-11.6.9/src/SystemUnit.java
@@ -0,0 +1,2 @@
+public class SystemUnit {
+}
