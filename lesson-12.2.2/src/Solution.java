52e934cead1edab96ffa1baee735b7f9e6abe73a
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-22 21:59:26 +0500

    Add files via upload

---
+++ lesson-12.2.2/src/Solution.java
@@ -0,0 +1,15 @@
+/*
+Земля: техническая характеристика
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Planet earth = new Planet();
+        earth.name = "Земля";
+        earth.age = 4_540_000_000L;
+        earth.speed = 170_218;
+        earth.area = 510_072_000;
+        earth.printInformation();
+    }
+}
