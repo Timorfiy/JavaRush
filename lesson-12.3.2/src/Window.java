a2d747cd7bbff20e14f8bfbabadb331b68017885
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-24 15:18:39 +0500

    Add files via upload

---
+++ lesson-12.3.2/src/Window.java
@@ -0,0 +1,19 @@
+/*
+Оконные работы
+*/
+
+public class Window {
+    private int height = 2;
+    private int width = 3;
+
+    public void changeSize(int height, int width){
+        this.height = height;
+        this.width = width;
+    }
+
+    public void printSize(){
+        System.out.println("Height = " + height);
+        System.out.println("Width = " + width);
+    }
+}
+
