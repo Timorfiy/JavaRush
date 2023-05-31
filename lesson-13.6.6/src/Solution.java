ef36e7f7f2bc88444d87532ffcd43861388681b0
Timofey <132925213+Timorfiy@users.noreply.github.com>
2023-05-31 21:48:28 +0500

    Add files via upload

---
+++ lesson-13.6.6/src/Solution.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static List<String> secretMessage = List.of(
+            "защ", "ита", "тит", "ити", "e ", "от ", "те ", "пож", "баг", "от ", "алу", "йст", "ужа", "сно", "й ",
+            "сны", "е ", "осв", "х г", "обо", "быс", "ала", "кти", "чны", "баг", "ско", "пле", "ов ", "рее", "ние",
+            "и ", "реш", "отп", "выз", "ен ", "уск", "вол", "те ", "нос", "ите", "каж", "нет", " из", "но ", "все",
+            " ин", "отк", "ите", "ста", "пож", "ар ", "мат", "алу", " бу", "риц", "рат.", "ыт.", "ы.");
+
+    public static void main(String[] args) {
+        List<String> decryptedMessage = decryptSecretMessage();
+
+        for (String s : decryptedMessage) {
+            System.out.print(s);
+        }
+    }
+
+    public static List<String> decryptSecretMessage() {
+        //напишите тут ваш код
+        List<String> message = new ArrayList<>();
+        for (int i = 0; i < secretMessage.size(); i++) {
+            if(i % 3 == 0)
+                message.add(secretMessage.get(i));
+        }
+        return message;
+    }
+}
